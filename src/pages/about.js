0c41fe3170ee291ba7b9718e7ee61fa0ff8ec56e
Alex Ocegueda <alexocegueda59@gmail.com>
2023-06-30 22:39:24 -0500

    Added imgs and descriptions to projects

--- src/pages/about.js
+++ src/pages/about.js
@@ -66,7 +66,7 @@ const Skill = styled.li`
 
 const Skills = styled.ul`
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
     width: 35%;
-    gap: .1rem;
+    gap: .5rem;
 `;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,38 @@ import './global.css';
 import Navbar from "../components/Navbar";
 import Project from "../components/Project"
 import styled from "styled-components";
+
 import cpuImg from "../images/cpu.jpg"
+import databaseImg from "../images/database.jpg";
+import pokeImg from "../images/pokemon.jpg";
+import pyScriptImg from "../images/pythonscripts.jpg";
 
 const IndexPage = () => {
 
   const cpuDescription = (
       <p>
-        Uses a 3 layered priority queue algoritm to simulate how a CPU schudules
-        incoming tasks. Uses Round Robin and First Come First Serve algoritms as well.
+        Uses a 3 layered priority queue algorithm to simulate how a CPU schudules
+        incoming tasks. Utilizes Round Robin and First Come First Serve algoritms as well.
       </p>
+  );
+
+  const pokeDescription = (
+    <p>
+      Built mobile friendly website to display original 151 pokemon.
+    </p>
+  );
+
+  const studentDescription = (
+    <p>
+      This is a SpringBoot project of a student database. 
+      It is connected with an external SQL database and performs basic CRUD commands. 
+    </p>
+  );
+
+  const pyScriptDescription = (
+    <p>
+      Collection of Python scripts i've done in and outside of college.
+    </p>
   );
 
   return (
@@ -20,11 +43,11 @@ const IndexPage = () => {
       <h1>Projects</h1>
       <Projects>
         <Project
-          displayName="Sample Project"
-          tools={['Python', 'Java']}
-          link="https://example.com"
-          description={cpuDescription}
-          imageSrc={cpuImg}
+          displayName="PokeAPI Pokedex"
+          tools={['React', 'JavaScript']}
+          link="https://github.com/AlexOcegueda/pokeapi_pokedex"
+          description={pokeDescription}
+          imageSrc={pokeImg}
         />
         <Project
           displayName="CPU Task Simulator"
@@ -34,11 +57,18 @@ const IndexPage = () => {
           imageSrc={cpuImg}
         />
         <Project
-          displayName="CPU Task Simulator"
-          tools={['Java']}
+          displayName="Python Scripts"
+          tools={['Python', 'Turtle', 'PyQt5']}
+          link="https://github.com/AlexOcegueda/Python_Scripts/"
+          description={pyScriptDescription}
+          imageSrc={pyScriptImg}
+        />
+        <Project
+          displayName="Student Database"
+          tools={['Java, SpringBoot, MySQL']}
           link="https://github.com/AlexOcegueda/MultiLevelQueues"
-          description={cpuDescription}
-          imageSrc={cpuImg}
+          description={studentDescription}
+          imageSrc={databaseImg}
         />
       </Projects>
     </>
