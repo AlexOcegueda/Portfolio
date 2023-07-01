9844a396409cb12e5cfc2dcf8be243e480ade04b
Alex Ocegueda <alexocegueda59@gmail.com>
2023-07-01 17:46:44 -0500

    quick fix resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 
-//import ResumePDF from '../../public/static/resume.pdf';
+import ResumePDF from '../../public/static/resume.pdf';
 
 const Navbar = () => {
     const handleResumeClick = () => {
@@ -15,7 +15,7 @@ const Navbar = () => {
             <NavLinks className='nav-links'>
                 <NavLink to="/about">About</NavLink>
                 <NavLink to="/">Projects</NavLink>
-                {/*<ResumeButton onClick={handleResumeClick}>Resume</ResumeButton>*/}
+                <ResumeButton onClick={handleResumeClick}>Resume</ResumeButton>
             </NavLinks>
         </Nav>
     );
