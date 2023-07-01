149e9a43d331748acb0945d47218b7a37b9837bf
Alex Ocegueda <alexocegueda59@gmail.com>
2023-06-30 21:10:52 -0500

    Remove styling errors

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,7 @@ const Project = ({ displayName, tools, link, description, imageSrc }) => {
     return (
         <Container>
             <Text>
-                <h1>{displayName}</h1>
+                <h2>{displayName}</h2>
                 <Tools>{tools.join(', ')}, <Link href={link}>Github</Link></Tools>
                 <Description>{description}</Description>
             </Text>
@@ -24,17 +24,15 @@ const Text = styled.div`
 const Image = styled.img`
     border-radius: 1rem;
     object-fit: cover;
+    width: 20rem;
 `;
 
 const Container = styled.div`
     display: flex;
     width: 40rem;
     height: 15rem;
-    padding: 0.5rem;
-    background-color: lightgray;
-    border-radius: 0.5rem;
-    box-shadow: -1px -1px 0 black, 2px -2px 0 black, -2px 2px 0 black, 2px 2px 0 black; 
     margin-top: 2rem;
+    padding: 1rem;
 `;
 
 const Tools = styled.p`
