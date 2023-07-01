31d6ff5160841b40da0bbfd16d63ec7bba692d64
Alex Ocegueda <alexocegueda59@gmail.com>
2023-07-01 14:23:48 -0500

    Added metadata for SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
+  siteMetadata: {
+    title: 'Alex Ocegueda Castro',
+    description: 'Web Development Portfolio',
+    copyright: 'Copyright 2023 Alex Ocegueda Castro'
+  }
 }
