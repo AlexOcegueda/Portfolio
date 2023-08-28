31d099b38fe15f6af22a69b99e24e6124dbda9f8
Alex Ocegueda <alexocegueda59@gmail.com>
2023-08-27 23:04:51 -0500

    added mouse cursor follower

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,44 @@ const metaDescription = "My name is Alex Ocegueda Castro and I am a recent colle
 Added 1 rem to left margin to deal with smaller devices
 */
 const About = () => {
+    const [dotPosition, setDotPosition] = React.useState({ x: 0, y: 0 });
+
+    const circleRef = React.useRef(null);
+
+    const handleMouseMove = (e) => {
+        setDotPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    React.useEffect(() => {
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, []);
+
+    React.useEffect(() => {
+        const updateCirclePosition = () => {
+            if (circleRef.current) {
+            const circleRect = circleRef.current.getBoundingClientRect();
+            circleRef.current.style.left = dotPosition.x - circleRect.width / 2 + 24 + "px";
+            circleRef.current.style.top = dotPosition.y - circleRect.height / 2 + 24 + "px";
+        }
+        requestAnimationFrame(updateCirclePosition);
+    };
+
+    updateCirclePosition();
+    }, [dotPosition]);
+
     return (
         <>
+            <div className="circle-container">
+                <div
+                  className="circle"
+                  ref={circleRef}
+                ></div>
+                <div className="dot" style={{ left: dotPosition.x, top: dotPosition.y }}></div>
+            </div>
             
             <Navbar />
             <div className='about-container'>
